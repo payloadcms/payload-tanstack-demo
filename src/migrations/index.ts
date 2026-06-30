5e6a6db52f52030ac0438b024095ae53f4240a3b
Jake Fletcher <jacobsfletch@gmail.com>
2026-06-30 14:44:54 -0400

    run migrations

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260630_184436 from './20260630_184436';
+
+export const migrations = [
+  {
+    up: migration_20260630_184436.up,
+    down: migration_20260630_184436.down,
+    name: '20260630_184436'
+  },
+];
