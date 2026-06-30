4ddeb25d8f1b300967fdf6e602385a364e4b1744
Jake Fletcher <jacobsfletch@gmail.com>
2026-06-30 14:23:14 -0400

    add vercel pg adapter

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,4 +1,4 @@
-import { sqliteAdapter } from '@payloadcms/db-sqlite'
+import { vercelPostgresAdapter } from '@payloadcms/db-vercel-postgres'
 import sharp from 'sharp'
 import path from 'path'
 import { buildConfig, PayloadRequest, createLocalReq } from 'payload'
@@ -60,9 +60,9 @@ export default buildConfig({
   },
   // This config helps us configure global or default features that the other editors can inherit
   editor: defaultLexical,
-  db: sqliteAdapter({
-    client: {
-      url: process.env.DATABASE_URL || 'file:./payload-tanstack.db',
+  db: vercelPostgresAdapter({
+    pool: {
+      connectionString: process.env.POSTGRES_URL || process.env.DATABASE_URL,
     },
   }),
   collections: [Pages, Posts, Media, Categories, Users],
