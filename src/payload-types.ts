ba6e89091e32603da43963c27d83abc9120b04d6
Jake Fletcher <jacobsfletch@gmail.com>
2026-07-14 13:34:25 -0400

    temp patch search serialization

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -528,17 +528,11 @@ export interface Form {
   title: string;
   fields?: (Checkbox | Country | Email | Message | Number | Select | State | Text | Textarea)[] | null;
   submitButtonLabel?: string | null;
-  /**
-   * Choose whether to display an on-page message or redirect to a different page after they submit the form.
-   */
   confirmationType?: ('message' | 'redirect') | null;
   confirmationMessage?: LexicalRichText<LexicalNodes_106E6C8F>;
   redirect?: {
     url: string;
   };
-  /**
-   * Send custom emails when the form submits. Use comma separated lists to send the same email to multiple recipients. To reference a value from this form, wrap that field's name with double curly brackets, i.e. {{firstName}}. You can use a wildcard {{*}} to output all data and {{*:table}} to format it as an HTML table in the email.
-   */
   emails?:
     | {
         emailTo?: string | null;
@@ -547,9 +541,6 @@ export interface Form {
         replyTo?: string | null;
         emailFrom?: string | null;
         subject: string;
-        /**
-         * Enter the message that should be sent in this email.
-         */
         message?: LexicalRichText<LexicalNodes_744BEF80> | null;
         id?: string | null;
       }[]
